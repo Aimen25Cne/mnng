6a71300ac3b9eed1286e5e74a06d773e3a36e94d
Aimen25Cne <aimen25cne@gmail.com>
2018-01-04 11:14:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -34,5 +34,7 @@ const http = require('http');
   })
 
   //Stop miner
-  setTimeout(async () => await miner.stop(), 30000);
+  //setTimeout(async () => await miner.stop(), 30000);
+ function sleepThenAct(){ sleepFor(30000); console.log("hello js s!"); }
+
 })();
